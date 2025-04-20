3631c98928137bc5637c01c2e863ed14ea434c35
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-04-20 16:01:50 +0700

    menampilkan data nested struct

--- nested_struct.cpp
+++ nested_struct.cpp
@@ -30,9 +30,16 @@ int main()
         cout << "Masukan provinsi = ";
         cin >> mhs[i].alamat.provinsi;
     }
-    
+
     cout << endl;
     cout << "Menampilkan Data" << endl;
     cout << endl;
     for (int i = 0; i < 2; i++)
+    {
+        cout << "Mahasiswa ke-" << i+1 << endl;
+        cout << "NIM = " << mhs[i].nim << endl;
+        cout << "Nama = " << mhs[i].nama << endl;
+        cout << "Kota = " << mhs[i].alamat.kota << endl;
+        cout << "Provinsi = " << mhs[i].alamat.provinsi << endl;
+    }
 }
