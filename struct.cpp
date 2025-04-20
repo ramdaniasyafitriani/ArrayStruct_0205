9b7eede63f0f0ac8f48027c94869b3103a7b4687
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-04-20 15:41:45 +0700

    membuat struct

--- struct.cpp
+++ struct.cpp
@@ -23,4 +23,7 @@ int main()
     cout << "Menampilkan Data" << endl;
     cout << endl;
 
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Alamat = " << mhs.alamat << endl;
 }
