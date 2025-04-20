9b4899344aeeea7a510b2b71fb3719b3061a9a12
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-04-20 15:38:59 +0700

    membuat object struct

--- struct.cpp
+++ struct.cpp
@@ -7,3 +7,16 @@ struct Mahasiswa
     string nim;
     string alamat;
 };
+
+int main()
+{
+    // Membuat object struct
+    Mahasiswa mhs;
+    cout << "Masukan nim = ";
+    cin >> mhs.nim;
+    cout << "Masukan nama = ";
+    cin >> mhs.nama;
+    cout << "Masukan alamat = ";
+    cin >> mhs.alamat;
+    
+}
